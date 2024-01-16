395e0e24f5c0d5175d61318d4711e117b9721522
andrea <ngocanhhsgs@gmail.com>
2024-01-16 15:41:17 +0200

    first commit

---
+++ Part1.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+
+public class Part1 {
+public static void main(String[] args) {
+    // built-in data 
+    // array
+    int [] numbers  = {1,2,3};
+    // LinkedList
+    LinkedList<String> list = new LinkedList<>();
+    list.add(4);
+}
+    
+}

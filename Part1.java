3542923691e3677130c5ad6b699031362029984d
andrea <ngocanhhsgs@gmail.com>
2024-01-17 11:47:06 +0200

    data types

--- Part1.java
+++ Part1.java
@@ -1,13 +1,32 @@
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 public class Part1 {
 public static void main(String[] args) {
     // built-in data 
     // array
     int [] numbers  = {1,2,3};
+
     // LinkedList
     LinkedList<String> list = new LinkedList<>();
-    list.add(4);
+    list.add("item1");
+
+    // Stack
+    Stack<Integer> stack = new Stack<>();
+    stack.push(1);
+
+    // Queue
+    Queue<Integer> queue = new LinkedList<>();
+    queue.offer(1);
+    queue.offer(2);
+
+    // HashMap
+    HashMap<Integer, String> hashMap = new HashMap<>();
+    hashMap.put(1,"one");
+    hashMap.put(2, "two");
+    System.out.println(hashMap.get(2));
 }
     
 }
